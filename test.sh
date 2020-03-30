33a6a586aa5cd17d0551798c39e4f4cc6d956b59
nestor-mateus-globant <nestor.mateus@smiledirectclub.com>
2020-03-30 10:26:43 -0500

    test.sh

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+REPOSITORY_URI=663668850653.dkr.ecr.us-east-1.amazonaws.com/sdc-airflow:latest
+
+docker build docker-whale .
+docker tag docker-whale:latest $REPOSITORY_URI
+docker push $REPOSITORY_URI
