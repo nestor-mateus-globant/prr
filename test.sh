89c6fc04f39d4e01d87e4d6e3ebe957e1d0eb128
nestor-mateus-globant <nestor.mateus@smiledirectclub.com>
2020-04-02 09:43:31 -0500

    minimize size

--- test.sh
+++ test.sh
@@ -8,8 +8,8 @@ REPOSITORY_URI=663668850653.dkr.ecr.us-east-1.amazonaws.com/sdc-airflow:latest
 
 #docker system prune -a
 #docker rmi 15ede5808858
-#docker build --rm --build-arg PYTHON_DEPS="flask_oauthlib>=0.9" -t sdc/docker-airflow .
-docker build -t sdc/docker-airflow -f Dockerfile.dev .
+docker build --rm --build-arg PYTHON_DEPS="flask_oauthlib>=0.9" -t sdc/docker-airflow .
+#docker build -t sdc/docker-airflow -f Dockerfile.dev .
 docker images
-docker tag sdc/docker-airflow:latest $REPOSITORY_URI
-docker push $REPOSITORY_URI
+#docker tag sdc/docker-airflow:latest $REPOSITORY_URI
+#docker push $REPOSITORY_URI
