5983970a6fcc299df8f9dd8201031474d72d70e9
nestor-mateus-globant <nestor.mateus@smiledirectclub.com>
2020-04-02 09:44:27 -0500

    minimize size

--- test.sh
+++ test.sh
@@ -11,5 +11,5 @@ REPOSITORY_URI=663668850653.dkr.ecr.us-east-1.amazonaws.com/sdc-airflow:latest
 docker build --rm --build-arg PYTHON_DEPS="flask_oauthlib>=0.9" -t sdc/docker-airflow .
 #docker build -t sdc/docker-airflow -f Dockerfile.dev .
 docker images
-#docker tag sdc/docker-airflow:latest $REPOSITORY_URI
-#docker push $REPOSITORY_URI
+docker tag sdc/docker-airflow:latest $REPOSITORY_URI
+docker push $REPOSITORY_URI
