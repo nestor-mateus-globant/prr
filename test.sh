ecd8516585c4c4e789570ca0afa7b6224a6afe49
nestor-mateus-globant <nestor.mateus@smiledirectclub.com>
2020-04-02 00:24:04 -0500

    minimize size

--- test.sh
+++ test.sh
@@ -6,8 +6,8 @@
 REPOSITORY_URI=663668850653.dkr.ecr.us-east-1.amazonaws.com/sdc-airflow:latest
 
 
-docker system prune -a
-docker rmi 15ede5808858
+#docker system prune -a
+#docker rmi 15ede5808858
 docker build --rm --build-arg PYTHON_DEPS="flask_oauthlib>=0.9" -t puckel/docker-airflow .
 #docker build -t docker-whale .
 docker images
